99fece8c989bc6573d018b6d38b3c38219d80134
Godsfavour <godsfavours@gmail.com>
2020-12-23 05:13:22 -0600

    Add and edit landmarks

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Landmark = require('./models/landmarks');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/land-view', {
   useNewUrlParser: true,
@@ -20,6 +21,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'pages'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 app.listen(8080, () => {
   console.log("Listening on port 8080 (localhost:8080)");
 })
@@ -28,13 +32,32 @@ app.get('/', (req, res) => {
   res.render('home');
 })
 
+app.get('/landmarks/create', (req, res) => {
+  res.render('landmarks/create_landmark');
+});
+
+app.post('/landmarks', async (req, res) => {
+  const landmark = new Landmark(req.body.landmark);
+  await landmark.save();
+  res.redirect(`landmarks/${landmark._id}`);
+});
+
 app.get('/landmarks', async (req, res) => {
   const landmarks = await Landmark.find({});
   res.render('landmarks/all_landmarks', { landmarks })
 });
 
 app.get('/landmarks/:id', async (req, res) => {
-  const { id } = req.params;
-  const landmark = await Landmark.findById(id);
-  res.render('landmarks/landmark', { landmark })
+  const landmark = await Landmark.findById(req.params.id);
+  res.render('landmarks/display_landmark', { landmark })
 });
+
+app.get('/landmarks/:id/edit', async (req, res) => {
+  const landmark = await Landmark.findById(req.params.id);
+  res.render('landmarks/edit_landmark', { landmark })
+});
+
+app.put('/landmarks/:id', async (req, res) => {
+  await Landmark.findByIdAndUpdate(req.params.id, { ...req.body.landmark });
+  res.redirect(`/landmarks/${req.params.id}`);
+})
